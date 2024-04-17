63f9440fd06d5bec5b40dd41d6b2f83c1d97ca24
Dirk Brockmannn <brockmann.dirk@gmail.com>
2024-04-17 18:05:34 +0200

    mulh

--- src/meta.js
+++ src/meta.js
@@ -1,5 +1,6 @@
 export default {
-  "title": "Critically Inflammatory",
-  "subtitle": "A Forrest Fire Model",
-  "description": "This explorable illustrates, as a representative of a broad range of dynamic phenomena, a simple model for the spatial spread of forest fires and the dynamic patterns they generate."
+  "title": "Kritisch entzündlich",
+  "subtitle": "Ein Modell für Waldbrände",
+  "description": "Das Explorable beschreibt ein einfaches Modell zur Dynamik von Waldbränden. Auf ganz natürliche Art und Weise führen seltene Blitzeinschläge, langsamer Nachwuchs und lokale Brandausbreitung zu einem fraktalen Bewuchsmuster wie man es aus der Natur kennt.",
+  
 }
--- src/parameters.js
+++ src/parameters.js
@@ -4,15 +4,18 @@ export default {
 		burntime: 10,
 		vegetation_growth_rate: {
 			range:[0,0.1],
-			default:0.05
+			default:0.05,
+			label:"Vegetationswuchs Geschwindigkeit"
 		},
 		lightning_rate: {
 			range:[0,1],
-			default: 0.25
+			default: 0.25,
+			label:"Blitzeinschlag Häufigkeit"
 		},
 		ignition_probability:{
 			range : [0,1],
-			default : 0.5
+			default : 0.5,
+			label:"Zündwahrscheinlichkeit"
 		}
 }
 
